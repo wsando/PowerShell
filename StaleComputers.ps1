81b80bd4592355a8e879990ad4fe8359d0e522b6
wsando <99703724+wsando@users.noreply.github.com>
2024-12-18 14:00:12 -0500

    Update StaleComputers.ps1
    updated to enumerate the login information from all domain controllers

--- StaleComputers.ps1
+++ StaleComputers.ps1
@@ -1,4 +1,5 @@
-# This script will ask how many days old and return a list of computers that have not logged in since then
+# This script will ask how many days old and return a list of computers that have not logged in, this will query all domain controllers to compile the list
+
 
 # Import the Active Directory module
 Import-Module ActiveDirectory
@@ -9,17 +10,34 @@ $days = Read-Host -Prompt "Enter the number of days to find stale computer accou
 # Calculate the cutoff date
 $cutoffDate = (Get-Date).AddDays(-[int]$days)
 
-# Get all computer accounts from Active Directory
-$computers = Get-ADComputer -Filter * -Property Name, LastLogonDate
+# Get all domain controllers in the domain
+$domainControllers = Get-ADDomainController -Filter *
 
 # Create an array to store the results
 $results = @()
 
+# Get all computer accounts from Active Directory
+$computers = Get-ADComputer -Filter * -Property Name
+
 foreach ($computer in $computers) {
-    if ($computer.LastLogonDate -lt $cutoffDate -or !$computer.LastLogonDate) {
+    $lastLogon = $null
+
+    # Query each domain controller for the precise last logon time
+    foreach ($dc in $domainControllers) {
+        $logonInfo = Get-ADComputer -Identity $computer.DistinguishedName -Server $dc.HostName -Properties lastLogon
+        if ($logonInfo.lastLogon) {
+            $dcLastLogon = [DateTime]::FromFileTime($logonInfo.lastLogon)
+            if (-not $lastLogon -or $dcLastLogon -gt $lastLogon) {
+                $lastLogon = $dcLastLogon
+            }
+        }
+    }
+
+    # Check if the computer is stale
+    if (-not $lastLogon -or $lastLogon -lt $cutoffDate) {
         $results += [PSCustomObject]@{
             ComputerName = $computer.Name
-            LastLogonDate = $computer.LastLogonDate
+            LastLogonDate = $lastLogon
         }
     }
 }
